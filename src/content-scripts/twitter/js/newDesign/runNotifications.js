8d9bf18be275b3e29a3e42d028b1122648092bdb
Danny Aziz <daanyaal997@gmail.com>
2019-09-19 17:49:23 +0100

    Refactor getting tweets from notifications stream

--- src/content-scripts/twitter/js/newDesign/runNotifications.js
+++ src/content-scripts/twitter/js/newDesign/runNotifications.js
@@ -2,12 +2,7 @@ import OnTweetsInjector from './OnTweetsInjector';
 
 const runNotifications = async (settings, api) => {
     if (settings.showScoreOnTweets) {
-        const articleElements = document.getElementsByTagName('article');
-        const tweetElements = [...articleElements].filter(item => {
-            return item.firstElementChild.lastElementChild.hasAttribute('data-testid');
-        });
-
-        // tweetElements = document.querySelectorAll('[data-testid=UserCell]');
+        const tweetElements = document.querySelectorAll('[data-testid=tweet]');
 
         const tInjector = new OnTweetsInjector(settings, api, tweetElements);
         await tInjector.run();
