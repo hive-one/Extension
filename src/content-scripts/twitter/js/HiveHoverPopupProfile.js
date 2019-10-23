688fdba86eb895e28e90ac52b4318c7145492350
Danny Aziz <daanyaal997@gmail.com>
2019-10-23 16:56:11 +0100

    Fix darkMode on injected popup

--- src/content-scripts/twitter/js/HiveHoverPopupProfile.js
+++ src/content-scripts/twitter/js/HiveHoverPopupProfile.js
@@ -25,10 +25,10 @@ const createHiveHoverPopupProfile = async (
 
         popupNode = document.createElement('div');
         popupNode.id = popupId;
-        // popupNode.classList.add('HiveExtension-Twitter_popup-profile');
-        // if (settings.isDarkTheme) {
-        //     popupNode.classList.add(`HiveExtension-Twitter_popup-profile-dark`);
-        // }
+        popupNode.classList.add('HiveExtension-Twitter_popup-profile-injection');
+        if (settings.isDarkTheme) {
+            popupNode.classList.add(`HiveExtension-Twitter_popup-profile-dark`);
+        }
         for (const key in popupStyles) {
             popupNode.style[key] = popupStyles[key];
         }
