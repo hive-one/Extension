ab51a3b734eeeebbf05d226cb980f6cae76c6dc2
Danny Aziz <daanyaal997@gmail.com>
2019-11-06 08:38:05 +0100

    Puppeteer setup, login with twitter

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,6 +1,37 @@
 /* eslint-env jest */
 
-test('This is just a test baby', async () => {
-    await page.goto('https://google.com');
-    expect(1 + 1).toBe(2);
+describe('Test Authenticated Extension', () => {
+    // Login
+    beforeAll(async () => {
+        await page.goto('https://twitter.com/login');
+        await page.type('[name="session[username_or_email]"]', 'USERNAMEHERE');
+        await page.type('input[class="js-password-field"]', 'PASSWORD HERE');
+        const form = await page.$("form[class='t1-form clearfix signin js-signin']");
+        await form.evaluate(form => form.submit());
+        await page.waitForNavigation();
+    });
+
+    it('Visit aantonop and check if extension exists in profile header', async () => {
+        await page.goto('https://twitter.com/aantonop');
+        let hiveElem = await page.waitForSelector(
+            'div[class="HiveExtension_Tooltip HiveExtension_Twitter_ProfileNav-container"]',
+        );
+        expect(await hiveElem.evaluate(node => node.className)).toBe(
+            'HiveExtension_Tooltip HiveExtension_Twitter_ProfileNav-container',
+        );
+    });
+
+    // Check the popup appears when clicking on header circle
+
+    // Check extension works on tweets
+
+    // Check popup appears when clicking on score/rank in tweet
+
+    // Check extension works on inline profile previews
+
+    // Check popup appears when clicking on score/rank inline profile previews
+
+    // Check popup injects on hover profile previews
+
+    // Check specifc rotues
 });
